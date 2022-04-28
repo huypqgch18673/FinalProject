ecaf2eff8f034e759a75841db7d5c23a84cb2b59
huypqgch18673 <huypqgch18673@fpt.edu.vn>
2022-04-28 13:34:42 +0700

    CodeHuyLong

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Category;
 use App\Form\CategoryType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,82 +14,88 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/category')]
 class CategoryController extends AbstractController
 {
-    #[Route('',name:'category_index')]
-    public function categoryIndex(){
+    #[Route('', name: 'category_index')]
+    public function categoryIndex()
+    {
 
         $category = $this->getDoctrine()->getRepository(Category::class)->findAll();
-        return $this->render("category/index.html.twig",[
+        return $this->render("category/index.html.twig", [
             'category' => $category
         ]);
     }
-    #[Route('/detail/{id}',name:'category_detail')]
-    public function categoryDetail($id){
+    #[Route('/detail/{id}', name: 'category_detail')]
+    public function categoryDetail($id)
+    {
         $category = $this->getDoctrine()->getRepository(Category::class)->find($id);
-        if($category == null){
-            $this->addFlash("Error","Category not found !");
+        if ($category == null) {
+            $this->addFlash("Error", "Category not found !");
             return $this->redirectToRoute("category_index");
         }
-        return $this->render("category/detail.html.twig",
-        [
-            'category' => $category
-        ]);
+        return $this->render(
+            "category/detail.html.twig",
+            [
+                'category' => $category
+            ]
+        );
     }
 
-    #[Route('delete/{id}',name:'category_delete')]
-    public function categoryDelete($id) {
+    #[Route('delete/{id}', name: 'category_delete')]
+    public function categoryDelete(ManagerRegistry $registry, $id)
+    {
         $category = $this->getDoctrine()->getRepository(Category::class)->find($id);
-        // if($category == null)
-        // {
-        //     $this->addFlash("Error","Category is not found !!");
+        if ($category == null) {
+            $this->addFlash("Error", "Category is not found !!");
+        }
+        // else if (count( $category->getProduct()) >=1) {
+        // $this->addFlash("Error","Can not delete Category !");
         // }
-        // else if (count( $category->getCategory()) !=0) {
-        //     $this->addFlash("Error","Can not delete Category !");
-        // }
-        // else {
-            $manager= $this->getDoctrine()->getManager();
+        else {
+            $manager = $registry->getManager();
             $manager->remove($category);
             $manager->flush();
-            $this->addFlash("Success","Delete author succeed !");
-        // }
+            $this->addFlash("Success", "Delete author succeed !");
+        }
         return $this->redirectToRoute("category_index");
     }
 
-    #[Route('/add',name:'category_add')]
+    #[Route('/add', name: 'category_add')]
     public function categoryAdd(Request $request)
     {
-        $category= new Category();
-        $form = $this->createForm(CategoryType::class,$category);
-        $form ->handleRequest($request);
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $manager=$this->getDoctrine()->getManager();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
             $manager->persist($category);
             $manager->flush();
             return $this->redirectToRoute("category_index");
         }
-        return $this->renderForm("category/add.html.twig",
-        [
-            'categoryForm' =>$form
-        ]);
-
+        return $this->renderForm(
+            "category/add.html.twig",
+            [
+                'categoryForm' => $form
+            ]
+        );
     }
 
-    #[Route('/edit{id}',name:'category_edit')]
-    public function categoryEdit(Request $request,$id){
+    #[Route('/edit{id}', name: 'category_edit')]
+    public function categoryEdit(Request $request, $id)
+    {
         $category = $this->getDoctrine()->getRepository(Category::class)->find($id);
-        $form= $this->createForm(CategoryType::class,$category);
+        $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $manager=$this->getDoctrine()->getManager();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
             $manager->persist($category);
             $manager->flush();
             return $this->redirectToRoute("category_index");
         }
-        return $this->renderForm("category/edit.html.twig",
-        [
-            'categoryForm'=>$form
-        ]);
+        return $this->renderForm(
+            "category/edit.html.twig",
+            [
+                'categoryForm' => $form
+            ]
+        );
     }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Product;
 use App\Form\ProductType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,85 +14,88 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/product')]
 class ProductController extends AbstractController
 {
-    #[Route('',name:'product_index')]
-    public function productIndex(){
+    #[Route('', name: 'product_index')]
+    public function productIndex()
+    {
 
         $product = $this->getDoctrine()->getRepository(Product::class)->findAll();
 
-        return $this->render("product/index.html.twig",[
+        return $this->render("product/index.html.twig", [
             'products' => $product,
-            
+
         ]);
     }
-    #[Route('/detail/{id}',name:'product_detail')]
-    public function productDetail($id){
+    #[Route('/detail/{id}', name: 'product_detail')]
+    public function productDetail($id)
+    {
         $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
-        if($product == null){
-            $this->addFlash("Error","Product not found !");
+        if ($product == null) {
+            $this->addFlash("Error", "Product not found !");
             return $this->redirectToRoute("product_index");
         }
-        return $this->render("product/detail.html.twig",
-        [
-            'product' => $product
-        ]);
+        return $this->render(
+            "product/detail.html.twig",
+            [
+                'product' => $product
+            ]
+        );
     }
 
-    
-    #[Route('delete/{id}',name:'product_delete')]
-    public function productDelete($id) {
-        $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
-        if($product == null)
-        {
-            $this->addFlash("Error","product is not found !!");
-        }
-        else if (count( $product->getProduct()) !=0) {
-            $this->addFlash("Error","Can not delete product !");
-        }
-        else {
-            $manager= $this->getDoctrine()->getManager();
+
+    #[Route('delete/{id}', name: 'product_delete')]
+    public function productDelete(ManagerRegistry $registry, $id)
+    {
+        $product = $registry->getRepository(Product::class)->find($id);
+        // $category = $registry->getRepository(Category::class)->findAll();
+        if ($product == null) {
+            $this->addFlash("Error", "product is not found !!");
+        } else {
+            $manager = $this->getDoctrine()->getManager();
             $manager->remove($product);
             $manager->flush();
-            $this->addFlash("Success","Delete author succeed !");
+            $this->addFlash("Success", "Delete author succeed !");
         }
         return $this->redirectToRoute("product_index");
     }
-    
-    #[Route('/add',name:'product_add')]
+
+    #[Route('/add', name: 'product_add')]
     public function productAdd(Request $request)
     {
-        $product= new Product;
-        $form = $this->createForm(ProductType::class,$product);
-        $form ->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $manager=$this->getDoctrine()->getManager();
-            $manager->persist($product);
-            $manager->flush();
-            return $this->redirectToRoute("product_index");
-        }
-        return $this->renderForm("product/add.html.twig",
-        [
-            'productForm' =>$form,
-            
-        ]);
-
-    }
-    #[Route('/edit{id}',name:'product_edit')]
-    public function productEdit(Request $request,$id){
-        $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
-        $form= $this->createForm(ProductType::class,$product);
+        $product = new Product;
+        $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $manager=$this->getDoctrine()->getManager();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
             $manager->persist($product);
             $manager->flush();
             return $this->redirectToRoute("product_index");
         }
-        return $this->renderForm("product/edit.html.twig",
-        [
-            'productForm'=>$form
-        ]);
+        return $this->renderForm(
+            "product/add.html.twig",
+            [
+                'productForm' => $form,
+
+            ]
+        );
+    }
+    #[Route('/edit{id}', name: 'product_edit')]
+    public function productEdit(Request $request, $id)
+    {
+        $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($product);
+            $manager->flush();
+            return $this->redirectToRoute("product_index");
+        }
+        return $this->renderForm(
+            "product/edit.html.twig",
+            [
+                'productForm' => $form
+            ]
+        );
     }
 }
